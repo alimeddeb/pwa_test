ab1237f44bbe5130492ef7f8f030b7466f1e4b1a
Ali Meddeb <medde001@umn.edu>
2020-09-24 18:16:40 +0100

    added fetch and search wather data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { fetchWeather } from './api/fetchWeather'
 import './App.css'
 
 
 const App = () => {
+    const [query, setQuery] = useState('');
+
+    const search = async(e) => {
+        if (e.key='Enter'){
+            const data = await fetchWeather(query);
+            console.log(data);
+        }
+    }
+
+
     return (
-        <h1>Test App</h1>
+        <div class='main-container'>
+            <input
+                type='text'
+                className='search'
+                placeholder='...'
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyPress={search}
+            />
+        </div>
     );
 }
 export default App;
