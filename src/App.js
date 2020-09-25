21f12c3681c172bbb88d2beb4839c65c48539c6c
Ali Meddeb <medde001@umn.edu>
2020-09-25 22:36:54 +0100

    adding back index

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ const App = () => {
         }
     }
     return (
+        
         <div className='main-container'>
-            <input type='text' className='search' placeholder='Search...' value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search}/>
+
             {weather.main && (
                 <div className='city'>
                     <h2 className='city-name'>
